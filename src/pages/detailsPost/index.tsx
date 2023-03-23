f0534372bf207a77fb9d95992b570bb8adea17ab
Leandro <leandromllr9@gmail.com>
2023-03-23 15:20:46 -0300

    feat: criação da page para detalhamento dos posts

---
+++ src/pages/detailsPost/index.tsx
@@ -0,0 +1,18 @@
+import Menu from '../../componentes/menu'
+import Post from '../../componentes/post'
+import styles from './index.module.css'
+
+const DetailsPost = ()=>{
+    return (
+        <section>
+            <Menu/>
+            <div>
+                <Post/>
+            </div>
+            <div>
+            </div>
+        </section>
+    )
+}
+
+export default DetailsPost
