8df45048bc990d733fd3b8478b1bf6622bdfe268
Leandro <leandromllr9@gmail.com>
2023-03-23 15:21:18 -0300

    feat: criação do componente post

---
+++ src/componentes/post/index.tsx
@@ -0,0 +1,19 @@
+import styles from './index.module.css'
+
+const Post = ()=>{
+    return (
+        <main className={styles.container}>
+            <div className={styles.titlePost}>
+                <h1>sunt aut facere repellat provident occaecati excepturi optio reprehenderit</h1>
+            </div>
+            <div className={styles.contentPost}>
+                <p>
+                    laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium
+                    
+                </p>
+            </div>
+        </main>
+    )
+}
+
+export default Post
