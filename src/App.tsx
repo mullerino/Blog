c2901ea6e15b996c38e094eadbbe4e8f613e275f
Leandro <leandromllr9@gmail.com>
2023-03-24 11:40:23 -0300

    feat: consumindo rota dos posts e criando rotas para cada post

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,42 @@
 import { Routes, Route } from 'react-router-dom'
+
+import axios from 'axios' 
+
 import Posts from "./pages/posts"
 import Users from './pages/users'
+import DetailsPost from './pages/detailsPost'
+import DetailsUser from './pages/detailsUser'
+
+import { IPost } from './@types/post'
+import { useEffect, useState } from 'react'
 
 function App() {
+  
+  const [posts,setPosts] = useState<IPost[]>([])
+
+  useEffect(()=>{
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+    .then((response)=>{
+      setPosts(response.data)
+    })
+    .catch((error)=>{
+      console.log(error)
+    })
+  }, [])
+
+
   return (
     <Routes>
-      <Route path="/" element = {<Posts/>}/>
+      <Route path="/" element = {<Posts posts={posts}/>}/>
       <Route path="/users" element = {<Users/>}/>
+      {posts.map((post)=>(
+        <Route 
+        key={post.id} 
+        path = {`/post/${post.id}`} 
+        element = {<DetailsPost body={post.body} title={post.title} id={post.id}/>}
+        />
+      ))}
+      <Route path='/user/1' element ={<DetailsUser/>}/>
     </Routes>
   )
 }
