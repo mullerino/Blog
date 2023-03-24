8854d8703b574e23ea35f1e32d701f56789a6b9d
Leandro <leandromllr9@gmail.com>
2023-03-23 22:03:59 -0300

    feat: criação do componente infoUser

---
+++ src/componentes/infoUser/index.tsx
@@ -0,0 +1,17 @@
+import styles from './index.module.css'
+
+interface IInfos {
+    infoRequest: string,
+    infoAnswer: string
+}
+
+const InfoUser = ( {infoRequest, infoAnswer} : IInfos )=>{
+    return (
+        <div className={styles.container}>
+            <span>{infoRequest}</span>
+            <span>{infoAnswer}</span>
+        </div>
+    )
+}
+
+export default InfoUser
